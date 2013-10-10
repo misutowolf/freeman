3f80493a592e0417cf91bdc380dd81dc324fee7e
Charles Baker <assemblag3@gmail.com>
2013-10-10 09:04:04 -0400

    Started work with SourceSocket

--- lib/source_socket.rb
+++ lib/source_socket.rb
@@ -1,5 +1,19 @@
+require 'source_address'
+require 'source_port'
+
 class SourceSocket
 
-  #TODO:  SOCKET STUFF!
+  attr_accessor :addr, :port
+
+  def initialize(addr,port)
+
+     addr.is_a?(SourceAddress) ? @addr = addr.ip : ( @addr = nil; puts 'Error: Address argument is wrong type.' )
+     port.is_a?(SourcePort) ? @port = port.num : ( @port = nil; puts 'Error: Port argument is wrong type.' )
+
+  end
+
+  def to_s
+    "#{@addr}:#{@port}"
+  end
 
 end
---
+++ spec/source_socket_spec.rb
@@ -0,0 +1,21 @@
+require 'rspec'
+require_relative '../lib/source_socket'
+
+describe '#socket' do
+
+  it '  - should take a SourceAddress and SourcePort object as parameters, returning a SourceSocket object' do
+    @addr = SourceAddress.new '127.0.0.1'
+    @port = SourcePort.new 27016
+    @sock = SourceSocket.new @addr, @port
+    @sock.addr.should eql '127.0.0.1'
+    @sock.port.should eql 27016
+  end
+
+  it '  - should output "address:port" when printed as a string' do
+    @addr = SourceAddress.new '127.0.0.1'
+    @port = SourcePort.new 27017
+    @sock = SourceSocket.new @addr, @port
+    @sock.to_s.should eql '127.0.0.1:27017'
+  end
+
+end
