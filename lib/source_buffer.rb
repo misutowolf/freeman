2bfb81212a8f1333079003c03a97d7f4594c0365
Charles Baker <assemblag3@gmail.com>
2013-10-11 14:07:06 -0400

    Wrote implementation plans for SourceBuffer for later.

--- lib/source_buffer.rb
+++ lib/source_buffer.rb
@@ -13,4 +13,20 @@ class SourceBuffer
 
   # TODO: Implement buffer that will deal with SourceSocket.
 
+
+
+  # TODO: Methods that need to be implemeneted:
+  #
+  #   * set(buffer) - Takes SourceBuffer object
+  #   * reset()
+  #   * remaining_bytes()
+  #   * get(length) - Takes amount of bytes to get from buffer.
+
+  #   SPECIFIC DATA TYPES (unpack from front of buffer)
+  #   * get_byte()  - Gets first byte from buffer.
+  #   * get_short() - Gets two bytes from buffer
+  #   * get_float() - Gets four bytes from buffer (Floating decimal)
+  #   * get_ulong() - Gets four bytes from buffer (Unsigned Long Integer)
+  #   * get_string() - Gets a null-terminated string from buffer.  Will require a loop (can't unpack this!)
+
 end
